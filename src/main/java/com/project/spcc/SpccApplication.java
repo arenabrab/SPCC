583d299ee34f2bf3d0e06df63191e3a309e3b7f0
arenabrab <andrewbarbanera@gmail.com>
2018-02-21 21:11:34 -0500

    First commit

---
+++ src/main/java/com/project/spcc/SpccApplication.java
@@ -0,0 +1,12 @@
+package com.project.spcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpccApplication.class, args);
+	}
+}
---
+++ src/test/java/com/project/spcc/SpccApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project.spcc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpccApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
